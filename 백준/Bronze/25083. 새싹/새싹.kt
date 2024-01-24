3da08529f1a8ed926628eee417dc779fa4f6ffb2
woogi <38851229+fnql@users.noreply.github.com>
2024-01-24 23:08:11 +0900

    [Bronze V] Title: 새싹, Time: 88 ms, Memory: 12040 KB -BaekjoonHub

---
+++ 백준/Bronze/25083. 새싹/새싹.kt
@@ -0,0 +1,8 @@
+fun main(args:Array<String>) = with(System.`in`.bufferedReader()) {
+    print("         ,r'\"7\n" +
+            "r`-_   ,'  ,/\n" +
+            " \\. \". L_r'\n" +
+            "   `~\\/\n" +
+            "      |\n" +
+            "      |")
+}
